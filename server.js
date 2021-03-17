d9521e910be6212a82b3c0602a86c053d1af6f8c
Pwg21 <kcaskade@gmail.com>
2021-03-16 22:55:17 -0600

    adding to atlas db

--- server.js
+++ server.js
@@ -10,8 +10,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false,
 });
 
